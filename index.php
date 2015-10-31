f53bbc07dcedda448a0cfa8a2435acd5b5bb0713
Kevin Wirth <me@wirthdesign.com>
2015-10-30 21:48:54 -0400

    Added a post thumbnail snippet that actually works!

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <article class="post">
         <?php if ( has_post_thumbnail() ) { ?>
-	         <img src="<?php the_post_thumbnail(); ?>" class="thumbnail">
+          <?php
+          $thumb_id = get_post_thumbnail_id();
+          $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+          ?>
+	         <img src="<?php echo $thumb_url[0]; ?>" class="thumbnail">
          <?php } ?>
         <div class="post-details">
           <h3 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
--- single.php
+++ single.php
@@ -5,7 +5,11 @@
     <!-- POST 1 -->
     <div class="post">
       <?php if ( has_post_thumbnail() ) { ?>
-         <img src="<?php the_post_thumbnail(); ?>" class="thumbnail">
+        <?php
+        $thumb_id = get_post_thumbnail_id();
+        $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+        ?>
+         <img src="<?php echo $thumb_url[0]; ?>" class="thumbnail">
        <?php } ?>
       <div class="post-details">
         <h3 class="title"><?php the_title(); ?></h3>
